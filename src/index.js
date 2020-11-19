77062c7378d6d42b07385165f8325feb925f9fe5
Tim Daubenschütz <tim@daubenschuetz.de>
2020-11-19 16:47:48 +0100

    Implement set method

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 // @format
+const add = require("date-fns/add");
+
+// NOTE: 1d = 86400s
+const oneDay = 86400;
 const tableName = "sessions";
 const schema = `
   CREATE TABLE IF NOT EXISTS ${tableName}
@@ -23,6 +27,35 @@ module.exports = ({ Store }) => {
 
     createDb() {
       this.client.exec(schema);
+    }
+
+    set(sid, sess, cb) {
+      let age;
+      if (sess.cookie && sess.cookie.maxAge) {
+        // NOTE: `Max-age` property in cookie is in unit seconds
+        age = sess.cookie.maxAge;
+      } else {
+        // NOTE: In cases `Max-age` is not set on a cookie, we set expire to
+        // one day in the future.
+        age = oneDay;
+      }
+
+      const now = new Date();
+      const expire = add(now, { seconds: age }).toISOString();
+      const entry = { sid, sess: JSON.stringify(sess), expire};
+
+      const q = this.client.prepare(`
+        INSERT OR REPLACE INTO 
+          ${tableName}
+        VALUES
+          (
+            @sid,
+            @sess,
+            @expire
+          )
+      `).run(entry);
+
+      cb(null, q);
     }
   }
 
--- test/index_test.js
+++ test/index_test.js
@@ -3,6 +3,7 @@ const test = require("ava").serial;
 const sqlite = require("better-sqlite3");
 const session = require("express-session");
 const { unlinkSync } = require("fs");
+const differenceInSeconds = require("date-fns/differenceInSeconds");
 
 const SqliteStore = require("../src/index.js")(session);
 
@@ -47,3 +48,86 @@ test("if initialization can be run twice without any errors", t => {
 test("if error is thrown when client is missing from options", t => {
   t.throws(() => new SqliteStore());
 });
+
+test("if it saves a new session record", t => {
+  const db = new sqlite(dbName, dbOptions);
+  const s = new SqliteStore({
+    client: db
+  });
+
+  const sid = "123";
+  const sess = { cookie: { maxAge: 2000 }, name: "sample name" };
+  s.set(sid, sess, (err, rows) => {
+    t.assert(!err);
+    t.assert(rows);
+  });
+
+  const dbSess = db.prepare("SELECT * FROM sessions WHERE sid = ?").get(sid);
+  t.assert(dbSess.sess === JSON.stringify(sess));
+  t.assert(dbSess.sid === sid);
+  t.assert(
+    differenceInSeconds(new Date(dbSess.expire), new Date()) >= sess.cookie.maxAge - 5
+  );
+
+  t.teardown(teardown);
+});
+
+test("if it overwrites an already-existing session", t => {
+  const db = new sqlite(dbName, dbOptions);
+  const s = new SqliteStore({
+    client: db
+  });
+
+  const sid = "123";
+  const sess = { cookie: { maxAge: 2000 }, name: "sample name" };
+  s.set(sid, sess, (err, rows) => {
+    t.assert(!err);
+    t.assert(rows);
+  });
+
+  const dbSess = db.prepare("SELECT * FROM sessions WHERE sid = ?").get(sid);
+  t.assert(dbSess.sess === JSON.stringify(sess));
+  t.assert(dbSess.sid === sid);
+  t.assert(
+    differenceInSeconds(new Date(dbSess.expire), new Date()) >= sess.cookie.maxAge - 5
+  );
+
+  const sess2 = { cookie: { maxAge: 5000 }, name: "replaced name" };
+  s.set(sid, sess2, (err, rows) => {
+    t.assert(!err);
+    t.assert(rows);
+  });
+  const dbSess2 = db.prepare("SELECT * FROM sessions WHERE sid = ?").get(sid);
+  t.assert(dbSess2.sess === JSON.stringify(sess2));
+  t.assert(dbSess2.sid === sid);
+  t.assert(
+    differenceInSeconds(new Date(dbSess2.expire), new Date()) >= sess2.cookie.maxAge - 5
+  );
+
+  t.teardown(teardown);
+});
+
+test("if it saves a session with a missing maxAge too", t => {
+  const db = new sqlite(dbName, dbOptions);
+  const s = new SqliteStore({
+    client: db
+  });
+
+  const sid = "123";
+  const sess = { cookie: {}, name: "sample name" };
+  const oneDayAge = 86400;
+  s.set(sid, sess, (err, rows) => {
+    t.assert(!err);
+    t.assert(rows);
+  });
+
+  const dbSess = db.prepare("SELECT * FROM sessions WHERE sid = ?").get(sid);
+  t.assert(dbSess.sess === JSON.stringify(sess));
+  t.assert(dbSess.sid === sid);
+  t.assert(
+    differenceInSeconds(new Date(dbSess.expire), new Date()) >= oneDayAge - 5
+  );
+
+  t.teardown(teardown);
+});
+
